d58071866c7d17079162c373b123e08624837236
Praveen-Sharma00 <praveensharma.inbox@gmail.com>
2020-04-10 22:29:48 +0530

    Added Dashboards Cards

--- Covid-19-App/src/app/pages/home/home.page.ts
+++ Covid-19-App/src/app/pages/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
+
+  dashboardCards = [
+    {
+      count: 1213456,
+      color: 'secondary',
+      category: 'Total Cases',
+      icon: 'totalcase.svg'
+    },
+    {
+      count: 1213456,
+      color: 'primary',
+      category: 'Active Cases',
+      icon: 'activecase.svg'
+    },
+    {
+      count: 1213456,
+      color: 'success',
+      category: 'Last 7 days',
+      icon: 'india.svg'
+    },
+    {
+      count: 1213456,
+      color: 'danger',
+      category: 'Total deaths',
+      icon: 'nuclear.svg'
+    },
+    {
+      count: 1213456,
+      color: 'warning',
+      category: 'Fatality Rate',
+      icon: 'globe.svg'
+    },
+    {
+      count: 1213456,
+      color: 'medium',
+      category: 'Cured/Migrated',
+      icon: 'cured.svg'
+    }
+  ];
 
   ngOnInit() {
   }
