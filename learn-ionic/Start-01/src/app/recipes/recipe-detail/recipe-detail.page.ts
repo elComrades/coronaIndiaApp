5f6d31860234f42b68d27d9d64ccbc26ed50831b
Praveen Sharma <praveensharma.inbox@gmail.com>
2020-04-11 08:47:28 +0530

    Added Delete feature

--- learn-ionic/Start-01/src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ learn-ionic/Start-01/src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {RecipesService} from '../recipes.service';
 import {Recipe} from '../recipe.model';
+import {AlertController} from '@ionic/angular';
 
 
 @Component({
@@ -10,23 +11,39 @@ import {Recipe} from '../recipe.model';
     styleUrls: ['./recipe-detail.page.scss'],
 })
 export class RecipeDetailPage implements OnInit {
-     loadedRecipe: Recipe;
+    loadedRecipe: Recipe;
 
     constructor(
         private activatedRoute: ActivatedRoute,
-        private recipeService: RecipesService
+        private recipeService: RecipesService,
+        private router: Router,
+        private alertCtrl: AlertController
     ) {
     }
 
     ngOnInit() {
         this.activatedRoute.paramMap.subscribe(paramMap => {
             if (!paramMap.has('recipeId')) {
-                console.log('re : ', paramMap)
-                // redirect
+                this.router.navigate(['/recipes']);
                 return;
             }
             const recipeId = paramMap.get('recipeId');
             this.loadedRecipe = this.recipeService.getRecipe(recipeId.toString());
+        });
+    }
+
+    onDeleteRecipe() {
+        this.alertCtrl.create({
+            header: 'Are you sure ?',
+            message: 'Do you really want to delete recipe?',
+            buttons: [{text: 'Cancel', role: 'cancel'}, {
+                text: 'Delete', handler: () => {
+                    this.recipeService.deleteRecipe(this.loadedRecipe.id);
+                    this.router.navigate(['/recipes']);
+                }
+            }]
+        }).then(alertEl => {
+            alertEl.present();
         });
     }
 }
